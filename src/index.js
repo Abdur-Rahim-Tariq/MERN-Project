1afe4660afd1dcbba612ef834c26309e53ad7fa8
RHm <abdurrahimtariq.ds@gmail.com>
2025-11-13 08:52:01 +0500

    add basic project setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('This is the home page!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+  console.log(`App Link is  http://localhost:${port}`)
+})
